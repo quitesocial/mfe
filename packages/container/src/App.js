5a2d3a386be314122424f4529a4c32abd334c14b
Andrey Razdorskiy <quitesocial@yandex.ru>
2022-08-29 19:49:49 +0400

    added dashboard app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,17 @@
 import React, {
   lazy,
   Suspense,
+  useEffect,
   useState,
 } from 'react'
 import {
-  BrowserRouter,
+  Redirect,
+  Router,
   Route,
   Switch,
 } from 'react-router-dom'
+
+import { createBrowserHistory } from 'history'
 
 import {
   StylesProvider,
@@ -19,16 +23,25 @@ import Header from './components/Header'
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const history = createBrowserHistory()
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
   
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+  
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -40,11 +53,17 @@ export default () => {
               <Route path='/auth'>
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path='/dashboard'>
+                {
+                  !isSignedIn && <Redirect to='/' />
+                }
+                <DashboardLazy />
+              </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
